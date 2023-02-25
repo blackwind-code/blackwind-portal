59d434746736968dfc8945a0447a4470172a1d48
KyungWonPark <kw.park.person@gmail.com>
2023-02-25 22:15:02 +0900

    Fixed json tags, added print for debugging

--- internal/dns/common.go
+++ internal/dns/common.go
@@ -1,13 +1,13 @@
 package dns
 
 type DNS_Record struct {
-	Comment  string   `json:"comment,omitempty"`
-	Type     string   `json:"type,omitempty"`
-	Content  string   `json:"content,omitempty"`
-	Name     string   `json:"name,omitempty"`
-	Priority int      `json:"priority,omitempty"`
-	Proxied  bool     `json:"proxied,omitempty"`
-	Tags     []string `json:"tags,omitempty"`
-	TTL      int      `json:"ttl,omitempty"`
-	Id       string   `json:"omitempty"`
+	Comment  string   `json:"comment"`
+	Type     string   `json:"type"`
+	Content  string   `json:"content"`
+	Name     string   `json:"name"`
+	Priority int      `json:"priority"`
+	Proxied  bool     `json:"proxied"`
+	Tags     []string `json:"tags"`
+	TTL      int      `json:"ttl"`
+	Id       string   `json:"id,omitempty"`
 }
--- internal/dns/init.go
+++ internal/dns/init.go
@@ -19,8 +19,11 @@ func Init(app *pocketbase.PocketBase, secret string) {
 	Log = log.New(os.Stdout, "[dns]", log.Ldate|log.Ltime|log.Llongfile)
 
 	CLOUDFLARE_GLOBAL_KEY = os.Getenv("CLOUDFLARE_GLOBAL_KEY")
+	Log.Printf("CLOUDFLARE_GLOBAL_KEY: %v\n", CLOUDFLARE_GLOBAL_KEY)
 	CLOUDFLARE_X_AUTH_EMAIL = os.Getenv("CLOUDFLARE_X_AUTH_EMAIL")
+	Log.Printf("CLOUDFLARE_X_AUTH_EMAIL: %v\n", CLOUDFLARE_X_AUTH_EMAIL)
 	DOMAIN_ZONE_IDENTIFIER = os.Getenv("DOMAIN_ZONE_IDENTIFIER")
+	Log.Printf("DOMAIN_ZONE_IDENTIFIER %v\n", DOMAIN_ZONE_IDENTIFIER)
 
 	app.OnModelBeforeCreate().Add(DNSBeforeCreate)
 	app.OnModelBeforeUpdate().Add(DNSBeforeUpdate)
